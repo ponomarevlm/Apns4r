551c29e0dec97a23c11dcf47cbe94507697e8cba
Leonid Ponomarev <leonid@nulana.com>
2009-07-16 20:41:50 +0600

    Keep-alive sendserver, separating configuration

--- bin/sendserver.rb
+++ bin/sendserver.rb
@@ -1,12 +1,8 @@
 #!/usr/bin/env ruby
-require 'rubygems'
-require 'eventmachine'
-require 'base64'
 
 #add script's dir to require path
 $: << File.expand_path(File.dirname(__FILE__))+'/../lib/'
-require 'sender'
-require 'apncore'
+['rubygems', 'eventmachine', 'base64', 'sender', 'apncore', 'config'].each{|lib| require lib}
 
 # properly close all connections and sokets
 def stop
@@ -39,8 +35,15 @@ end
 
 EventMachine::run {
   if APNs4r::Sender.establishConnection :sandbox
+    # pinging our device to avoid socket close by APNs
+    EventMachine::add_periodic_timer( 300 ) do
+      p = { :ping => Time.now.to_i.to_s }
+      notification = APNs4r::Notification.new :payload => p.to_payload , \
+        :payload_length => p.payload_length, :device_token => OPTIONS[:ping_device_token]
+      APNs4r::Sender.send notification
+    end
     puts "#{Time.now.to_s} SendServer started"
-    $server = EventMachine::start_server "0.0.0.0", 8801, SendServer
+    $server = EventMachine::start_server OPTIONS[:sendserver_ip], OPTIONS[:sendserver_port], SendServer
   else
     puts "#{Time.now.to_s} SendServer: failed to connect to APNs: timeout"
     exit 1
---
+++ env/config.rb
@@ -0,0 +1,5 @@
+OPTIONS = { 
+  :ping_device_token => "0000000000000000000000000000000000000000000000000000000000000000".hex,
+  :sendserver_ip => 'localhost',
+  :sendserver_port => 8801
+  }
