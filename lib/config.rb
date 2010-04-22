161c5a89971acc33d9c0ac707885334bef6714a6
Manu Campos <manueltxo@gmail.com>
2010-04-22 18:13:49 +0200

    replaced RAILS_ROOT and RAILS_ENV by Rails.root and Rails.env To make it compatible with rails 3

--- lib/config.rb
+++ lib/config.rb
@@ -1,8 +1,8 @@
 OPTIONS =
-  if defined?(RAILS_ENV)
-    raw_config = File.read(RAILS_ROOT + "/config/apns4r.yml")
+  if defined?(Rails.env)
+    raw_config = File.read(Rails.root.join("config", "apns4r.yml")
     parsed_config = ERB.new(raw_config).result
-    YAML.load(parsed_config)[RAILS_ENV].symbolize_keys
+    YAML.load(parsed_config)[Rails.env].symbolize_keys
   else
     require 'erb'
     raw_config = File.read(File.expand_path(File.dirname(__FILE__)) + "/../apns4r.yml")
